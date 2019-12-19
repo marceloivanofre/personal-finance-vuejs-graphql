3e5e9de0f784580ef3502bc6a0f5bfa9b6e5190e
marceloivanofre <marceloivanofre13@gmail.com>
2019-12-18 23:51:00 -0200

    feat: add mutation login

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,6 +1,27 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const login = async (_, { email, password }, ctx, info) => {
+  const user = await ctx.db.query.user({ where: { email } });
+  if (!user) {
+    throw new Error('Invalid credentials');
+  }
+
+  const valid = await bcrypt.compare(password, user.password);
+  if (!valid) {
+    throw new Error('Invalid credentials');
+  }
+
+  const token = jwt.sign({ client_id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: '2h'
+  });
+
+  return {
+    token,
+    user
+  };
+};
+
 const signup = async (_, args, ctx, info) => {
   const password = await bcrypt.hash(args.password, 10);
   const user = await ctx.db.mutation.createUser({
@@ -21,5 +42,6 @@ const signup = async (_, args, ctx, info) => {
 };
 
 module.exports = {
+  login,
   signup
 };
